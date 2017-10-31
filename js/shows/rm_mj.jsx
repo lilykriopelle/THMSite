f4e204579a0f398febf8184fffa8e548abc8f47e
Lily Riopelle <lilykriopelle@gmail.com>
2017-10-31 16:36:18 -0400

    Add ticketing info to rmmj page.

--- js/shows/rm_mj.jsx
+++ js/shows/rm_mj.jsx
@@ -29,6 +29,16 @@ var RestaurantLysistrata = React.createClass({
           <div style={{marginTop: 20}}>
             This fall, Two Headed Rep’s pairing of plays find themselves in the workplace, inhabited by people who, every day, confront, consider, and carry out what’s expected of them. What weighs on us when we’re expected to do everything? What about when nothing is expected of us? And what do we do about the dog? Two Headed Rep asks these questions and more with their fall rep: a new adaptation of Miss Julie by Brittany Allen, Will Arbery, and Amanda Keating, and Reno & Moll, a new play written in response to Strindberg’s classic by Emma Horwitz.
           </div>
+
+          <div>
+            <p style={{fontSize: 50, marginBottom: 20}} className="centered">
+              <a href="https://www.artful.ly/two-headed-rep" target="_blank" className="highlight-link">tickets</a> and <a href="https://www.artful.ly/two-headed-rep/store/passes" target="_blank" className="highlight-link">passes</a> on sale now!
+            </p>
+
+            <img src={'images/Fall2017Horizontal.jpg'}/>
+
+            <p style={{marginTop: 20}}>{"We are so excited to announce that this fall, for the first time ever, you have the option of purchasing a Two Play Pass. What does that mean, exactly? It means you can buy one pass for just $23 that gets you into both shows on whatever day(s)/night(s) you want to see them! See old and new side by side in a single sitting, or come hang out with your favorite upstart theatre company on two separate occasions. Trust us: you'll have a great time either way."}</p>
+          </div>
         </section>
       </main>
     );
