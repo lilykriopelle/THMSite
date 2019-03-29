cbcd4f7bbcd27840251baf50e45de1911b8c6832
Lily Riopelle <lilykriopelle@gmail.com>
2019-03-29 16:40:09 -0400

    Add show previews to rep page.

--- bundle.js
+++ bundle.js
@@ -30528,31 +30528,9 @@
 	        ),
 	        React.createElement(
 	          'div',
-	          { style: { marginTop: 20 } },
-	          React.createElement(
-	            'div',
-	            { style: { fontWeight: "bold" } },
-	            'Karaoke at The Golden Sun Convention Center'
-	          ),
-	          React.createElement(
-	            'div',
-	            null,
-	            'This Merger is happening. Sandwiched between team building exercises and more team building exercises is a mandatory karaoke hour at The Golden Sun Convention Center where everyone has to sing for their job. While Lenny and Vince vamp onstage and Elena and Janet hide in Stairwell D, Big Boss Pete is in the back still working with the numbers and there’s not enough Svedka & OJ to assuage everyone’s existential panic: Who’s getting fired? Who’s staying hired? And which is worse? Don’t worry. No matter what happens, everyone gets tote bags, toothpicks, and a killer selection of midi files to take with them on the road to whatever comes next.'
-	          )
-	        ),
-	        React.createElement(
-	          'div',
-	          { style: { marginTop: 20 } },
-	          React.createElement(
-	            'div',
-	            { style: { fontWeight: "bold" } },
-	            'Leonce and Lena'
-	          ),
-	          React.createElement(
-	            'div',
-	            null,
-	            'When Prince Leonce learns that he\'s to wed Princess Lena – a woman he\'s never even laid eyes on – he and his BFF run away (to Italy!) to escape his fate and chase his ideals. Meanwhile, in another kingdom, Princess Lena learns she\'s to marry a prince named Leonce. Unwilling to be boxed in to the role of compliant wife to some dude she\'s never met and certainly doesn\'t love, she and her BFF run away (to Italy!) to escape her fate and chase her ideals. Oh, and this is a fairytale, so you can guess what happens next. Georg Büchner\'s raucous satire lampoons aristocracy, pseudo-philosophical bluster, and political echo chambers all while asking the question: who gets to rest easy knowing that fate will take care of everything in the end?'
-	          )
+	          { className: 'group', style: { marginTop: 20 } },
+	          React.createElement(ShowPreview, { classes: ["float-left"], title: 'LEONCE & LENA', imageUrl: 'images/ll_images/ll4.jpg', link: '/karaoke' }),
+	          React.createElement(ShowPreview, { classes: ["float-right"], title: 'KARAOKE AT THE GOLDEN SUN CONVENTION CENTER', imageUrl: 'images/karaoke_images/karaoke1.jpg', link: '/karaoke' })
 	        )
 	      )
 	    );
